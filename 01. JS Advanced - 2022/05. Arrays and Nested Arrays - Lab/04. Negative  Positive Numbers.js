c687ae9ff07c1a06f96a16dce805489f849e2821
Mario <mariovladimirovfs@gmail.com>
2022-10-06 16:55:53 +0300

    upload

---
+++ 01. JS Advanced - 2022/05. Arrays and Nested Arrays - Lab/04. Negative  Positive Numbers.js
@@ -0,0 +1,15 @@
+function negativePositiveNumbers(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+      result.unshift(array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  console.log(result.join(`\n`));
+}
+negativePositiveNumbers([7, -2, 8, 9]);
+console.log(`---`);
+negativePositiveNumbers([3, -2, 0, -1]);
