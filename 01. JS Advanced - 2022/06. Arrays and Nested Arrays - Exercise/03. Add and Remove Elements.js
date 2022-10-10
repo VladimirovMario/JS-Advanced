3cb75fb3b9a0860eda81c89f3d18cc0a72a86698
Mario <mariovladimirovfs@gmail.com>
2022-10-10 08:13:58 +0300

    upload

---
+++ 01. JS Advanced - 2022/06. Arrays and Nested Arrays - Exercise/03. Add and Remove Elements.js
@@ -0,0 +1,21 @@
+function addAndRemoveElements(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == `add`) {
+      result.push(i + 1);
+    } else {
+      result.pop();
+    }
+  }
+  if (result.length == 0) {
+    console.log(`Empty`);
+  } else {
+    console.log(result.join(`\n`));
+  }
+}
+addAndRemoveElements(["add", "add", "add", "add"]);
+console.log(`---`);
+addAndRemoveElements(["add", "add", "remove", "add", "add"]);
+console.log(`---`);
+addAndRemoveElements(["remove", "remove", "remove"]);
