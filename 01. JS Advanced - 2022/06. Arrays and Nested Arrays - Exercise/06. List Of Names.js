cf0e51af5ce6c624e6997ab4b687526029cba845
Mario <mariovladimirovfs@gmail.com>
2022-10-11 11:16:53 +0300

    upload

---
+++ 01. JS Advanced - 2022/06. Arrays and Nested Arrays - Exercise/06. List Of Names.js
@@ -0,0 +1,6 @@
+const listOfNames = (array) =>
+  array
+    .sort((a, b) => a.localeCompare(b))
+    .forEach((name, i) => console.log(`${i + 1}.${name}`));
+
+listOfNames(["John", "Bob", "Christina", "Ema"]);
