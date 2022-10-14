3764edce4d72d08a7442f981fdacf55e7153e44b
Mario <mariovladimirovfs@gmail.com>
2022-10-14 08:19:23 +0300

    upload

---
+++ 01. JS Advanced - 2022/07. Objects and Composition/01. City Record.js
@@ -0,0 +1,5 @@
+function cityRecord(name, population, treasury) {
+  return (object = { name, population, treasury });
+}
+console.log(cityRecord("Tortuga", 7000, 15000));
+console.log(cityRecord("Santo Domingo", 12000, 23500));
