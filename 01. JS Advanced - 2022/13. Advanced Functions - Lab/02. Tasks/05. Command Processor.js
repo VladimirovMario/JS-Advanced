67ebeb571efe92b7265b81e4001739b74cfd137a
VladimirovMario <mariovladimirovfs@gmail.com>
2022-11-11 06:57:47 +0200

    upload

---
+++ 01. JS Advanced - 2022/13. Advanced Functions - Lab/02. Tasks/05. Command Processor.js
@@ -0,0 +1,40 @@
+function solution() {
+  let state = ``;
+
+  function append(str) {
+    state += str;
+  }
+  function removeStart(num) {
+    state = state.substring(num);
+  }
+
+  function removeEnd(n) {
+    state = state.slice(0, -n);
+  }
+
+  function print() {
+    console.log(state);
+  }
+
+  return {
+    append,
+    removeStart,
+    removeEnd,
+    print,
+  };
+}
+let firstZeroTest = solution();
+
+firstZeroTest.append("hello");
+firstZeroTest.append("again");
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+let secondZeroTest = solution();
+
+secondZeroTest.append("123");
+secondZeroTest.append("45");
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
