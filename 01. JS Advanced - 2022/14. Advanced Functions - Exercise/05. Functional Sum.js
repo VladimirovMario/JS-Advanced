644b3416db29c193213ddc9a559abe03ed11672f
VladimirovMario <mariovladimirovfs@gmail.com>
2022-11-20 17:08:16 +0200

    upload

---
+++ 01. JS Advanced - 2022/14. Advanced Functions - Exercise/05. Functional Sum.js
@@ -0,0 +1,14 @@
+function add(sum) {
+  const inner = function (a) {
+    sum += a;
+    return inner;
+  };
+
+  inner.toString = function () {
+    return sum;
+  };
+
+  return inner;
+}
+console.log(add(1).toString());
+console.log(add(1)(6)(-3).toString());
