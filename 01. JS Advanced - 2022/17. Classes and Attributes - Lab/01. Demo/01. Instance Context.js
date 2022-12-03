92060903bb6fb3e82c2a374e18aa27e69d6cae22
VladimirovMario <mariovladimirovfs@gmail.com>
2022-12-03 18:02:16 +0200

    upload

---
+++ 01. JS Advanced - 2022/17. Classes and Attributes - Lab/01. Demo/01. Instance Context.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  displayName() {
+    console.log(`Name: ${this.firstName} ${this.lastName}`);
+  }
+}
+const person = new Person("John", "Doe");
+person.displayName(); // Name: John Doe
