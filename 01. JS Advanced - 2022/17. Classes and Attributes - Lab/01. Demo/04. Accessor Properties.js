fb0801d3b84a0dc60ed5af25533bd9ddb2d4c9cf
VladimirovMario <mariovladimirovfs@gmail.com>
2022-12-06 21:05:41 +0200

    upload

---
+++ 01. JS Advanced - 2022/17. Classes and Attributes - Lab/01. Demo/04. Accessor Properties.js
@@ -0,0 +1,41 @@
+1;
+class FirstCircle {
+  constructor(r) {
+    this.radius = r;
+  }
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
+}
+const circle = new FirstCircle(5);
+console.log(circle.area); // 78.5398…
+console.log(`----`);
+
+2;
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return 2 * this.radius;
+  }
+  //Accessors are often used for validation
+  //The setter can verify that a given value meets requirements
+  set diameter(value) {
+    if (value <= 0) {
+      throw new Error("Diameter must be positive");
+    }
+    this.radius = value / 2;
+  }
+
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+let c = new Circle(2);
+c.diameter = 1.6;
+console.log(`Radius: ${c.radius}`); // 0.8
+console.log(`Diameter: ${c.diameter}`); // 1.6
+console.log(`Area: ${c.area}`); // 2.0106…
